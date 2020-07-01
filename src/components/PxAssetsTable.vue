b9504a01bfa9fab557237a69d198b34ef4fc9bfc
Bernardo Ayala <bernardoayala2@gmail.com>
2020-06-30 22:13:18 -0400

    filters added

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -20,7 +20,13 @@
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
         <td>
-          <img class="h-16 w-16" :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`" :alt="a.name">
+          <img
+            class="h-6 w-6"
+            :src="
+              `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="a.name"
+          />
         </td>
         <td>
           <b>#{{ a.rank }}</b>
@@ -28,9 +34,17 @@
         <td>
           {{ a.name }}
         </td>
-        <td> {{ a.priceUsd }} </td>
-        <td> {{ a.marketCapUsd }} </td>
-        <td>{{ a.changePercent24Hr }}</td>
+        <td>{{ a.priceUsd | dollar }}</td>
+        <td>{{ a.marketCapUsd | dollar }}</td>
+        <td
+          :class="
+            a.changePercent24Hr.includes('-')
+              ? 'text-red-600'
+              : 'text-green-600'
+          "
+        >
+          {{ a.changePercent24Hr | percent }}
+        </td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
